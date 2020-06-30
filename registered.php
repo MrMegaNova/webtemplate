60f836860208f76b7de33eade1c2be22ab9b9888
MrMegaNova <contact@netsteel.space>
2020-06-30 11:33:21 +0200

    first commit

---
+++ registered.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+        <h2>Congrats, you've been registred !</h2>
+        <p>Welcome on our magic website.</p>
+        <script>
+            setTimeout(function(){
+                window.location.href = "http://127.0.0.1:8080/login.php";
+                }, 3000);
+        </script>
+</body>
+</html>
